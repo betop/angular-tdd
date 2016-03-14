996cc2eb9452da02a7cec6c5026ebb3bdc0edb49
ggm <stidpeace@gmail.com>
2016-03-14 21:21:44 +0200

    test service, controller, filter, directive

--- app/main.js
+++ app/main.js
@@ -1,1 +1,38 @@
-angular.module('AddressBook', []);
+angular.module('AddressBook', [])
+
+.service('contactService', function ($http) {
+  this.contacts = [];
+  var contactService = this;
+  $http.get('http://localhost:9001/contacts')
+    .then(function (res) {
+      console.log(res);
+      while (res.data[0]) {
+        contactService.contacts.push(res.data.pop());
+      }
+    });
+})
+
+.controller('ContactController', function (contactService, $scope) {
+  $scope.contacts = contactService.contacts;
+})
+
+.filter('proper', function () {
+  return function (name) {
+    var type = typeof name;
+    if (type !== 'number' && type !== 'string')
+      throw new Error();
+    return name.toString().split(" ").map(function (word) {
+      return word[0].toUpperCase().concat(word.slice(1));
+    }).join(' ');
+  };
+})
+
+.directive('avatar', function () {
+  return {
+    restrict: 'AE',
+    scope: {
+      name: '='
+    },
+    template: '<span class="avatar">{{name[0] | proper}}</span>'
+  }
+});
--- test/main.spec.js
+++ test/main.spec.js
@@ -1,5 +1,81 @@
-describe('The Address Book App', function() {
-  it('should work', function () {
-    chai.assert.isArray([]);
+var assert = chai.assert;
+var expect = chai.expect;
+
+describe('The Address Book App', function () {
+  describe('the contact service', function () {
+    beforeEach(function () {
+      module('AddressBook');
+      inject(function ($injector) {
+        contactService = $injector.get('contactService');
+        $httpBackend = $injector.get('$httpBackend');
+      });
+    });
+    
+    it ("should have a property contacts, an array", function () {
+      expect(contactService.contacts).to.be.an('array');
+    });
+
+    it ("should call the backend", function () {
+      $httpBackend.expectGET('http://localhost:9001/contacts')
+        .respond(200, []);
+      $httpBackend.flush();
+
+    });
+  });
+
+  describe('the contact controller', function () {
+    beforeEach(function () {
+      module('AddressBook');
+      inject(function ($injector, $rootScope) {
+        $scope = $rootScope.$new();
+        contactService = $injector.get('contactService');
+        $controller = $injector.get('$controller');
+      });
+    });
+
+    it ('should store an array of contacts in scope', function () {
+      $controller('ContactController', {$scope: $scope, contactService: contactService});
+      assert.isArray($scope.contacts);
+    });
+  });
+
+  describe('the proper filter', function () {
+    beforeEach(function () {
+      module('AddressBook');
+      inject(function ($injector) {
+        proper = $injector.get('$filter')('proper');
+      });
+    });
+
+    it ('should proper case a string', function () {
+      expect(proper('ned stark')).to.equal('Ned Stark');
+      expect(proper('cersei lannister')).to.equal('Cersei Lannister');
+    });
+
+    it ('should take a number and return that as a string', function () {
+      expect(proper(42)).equal('42');
+    });
+
+    it ('should throw an error on an incompatible type', function () {
+      assert.throws(function () {
+        proper(undefined);
+      });
+    });
+  });
+
+  describe('the avatar directive', function () {
+    beforeEach(function () {
+      module('AddressBook');
+    });
+
+    it ('should display the capitalized first lettter of a name', function () {
+      inject(function ($rootScope, $compile) {
+        $rootScope.contact = { name: 'jon arryn' };
+        var element = $compile('<avatar name=contact.name/>')($rootScope);
+        $rootScope.$digest();
+        var dirText = element.text();
+        expect(dirText).to.equal('J');
+      });
+    });
   });
 });
