0bd328c4c342eeba2a1e5dd39b92fa74e37d1d89
ggm <stidpeace@gmail.com>
2016-03-15 10:50:09 +0200

    end2end test

--- test/e2e/app.spec.js
+++ test/e2e/app.spec.js
@@ -1,5 +1,12 @@
 describe ('end-to-end address test', function () {
-  it ('should have contacts', function () {
+  it ('should have contacts', function (done) {
     browser.get('http://localhost:8080');
+    element.all(by.repeater('contact in contacts'))
+      .then(function(contacts) {
+        var first  = contacts[0];
+        var text = first.getText();
+        expect(text).toEqual('Steven S');
+        done();
+      });
   });
 });
